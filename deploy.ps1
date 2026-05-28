944f8267e2e99f3e9ecb52cc483b2eabe51effc8
revaddu <55227630+revaddu@users.noreply.github.com>
2026-05-28 18:53:42 +0200

    Remove deploy.ps1 from gh-pages

--- deploy.ps1
+++
@@ -1,9 +0,0 @@
-# Stop execution on any error
-$ErrorActionPreference = "Stop"
-
-# Create gh-pages branch from current files
-git checkout -b gh-pages
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:revaddu/snake-game-ts.git gh-pages
-git checkout main
